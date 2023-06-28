690d995e586ecc3f9625dfd388460affe836b053
bonney <quophiclef@gmail.com>
2023-06-28 01:31:45 -0700

    bed

--- src/pages/home/Bill.jsx
+++ src/pages/home/Bill.jsx
@@ -12,7 +12,7 @@ const Bill = () => {
             md={6}
             lg={6}
             className="mt-5 mb-3"
-            data-aos="zoom-out"
+            data-aos="fade-in"
             data-aos-duration="1000"
             data-aos-delay="100"
           >
@@ -28,7 +28,7 @@ const Bill = () => {
             md={6}
             lg={6}
             className="mt-5"
-            data-aos="zoom-out"
+            data-aos="fade-in"
             data-aos-duration="1000"
             data-aos-delay="200"
           >
