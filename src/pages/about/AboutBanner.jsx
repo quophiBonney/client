cb2c9e09f339cb0838490717793af7c629aa7a5a
bonney <quophiclef@gmail.com>
2023-07-03 03:31:08 -0700

    yes

--- src/pages/about/AboutBanner.jsx
+++ src/pages/about/AboutBanner.jsx
@@ -5,7 +5,7 @@ const AboutBanner = () => {
     <div className="container-fluid about-banner">
       <Container className="">
         <Row className="justify-content-center">
-          <Col sm={12} md={12} lg={7} className="text-center py-3">
+          <Col sm={12} md={12} lg={8} className="text-center py-3">
             <div>
               <h1 className="main-text">All About BuildBridge</h1>
               <p className="paragraph text-light mb-5">
